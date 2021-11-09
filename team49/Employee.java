c5a05d1c613118d3f41129e3b2ae13ede9a71a88
b1urr <93118753+1amzac@users.noreply.github.com>
2021-11-09 04:41:40 -0700

    Add files via upload
    Integrated login portion of GUI. User accounts are identified by a letter preceding their unique ID. Added checkPassword(), changeFile(), and getCurrentFileName() methods to the Database class. Added database default constructor which sets fileName to null. Cleaned up Patient class. Added password field in create account section of GUI,java. Implemented new patient object data storage with gui. GUI clears text fields and returns to login screen after account creation.

--- team49/Employee.java
+++ team49/Employee.java
@@ -2,8 +2,11 @@ package team49;
 
 public interface Employee {
 	
-	public void viewPatientHistory(Patient patient);
-	public void viewPatientContactInformation(Patient patient);
-	public void viewPreviousExamination(Patient patient);
+
+
+	
+	
+	
+	
 	
 }
--- team49/Patient.java
+++ team49/Patient.java
@@ -1,53 +1,26 @@
 package team49;
 
-
-
-
+import java.io.IOException;
 
 public class Patient {
 	
 	//This is the amount of data members that need to be stored in text file
-	final int arraySize = 18;
+	final int arraySize = 19;
 	
 	//Patient information variables
-	String firstName;
-	String lastName;
-	String patientID;  						//Format: P#####
-	String DOB;								//Format: Year-Month-Day
-	String phoneNumber;
-	String insuranceInformation;
-	String pharmacyInformation;	
+	public String firstName, lastName, patientID, DOB, phoneNumber, insuranceInformation, pharmacyInformation, 
+		   currentMedications, immunizations, currentHealthIssues, bloodPressure, currentAllergies,
+		   currentHealthConcerns, physicalExamResults;
 	int currentAge;
-	String currentMedications;
-	String immunizations;
-	String currentHealthIssues;
-	
-	
-	//login information
-	String username;
+	boolean isNew = true;
 	private String password;
-	
-	//Patient data from EnterVitals() in nurse class
-	double currentWeight; 			 		//pounds
-	double currentTemperature; 		 		//Fahrenheit
-	String bloodPressure;					 //Systolic/Diastolic  Ex: "120/80"
-	
-	//Patient data from setHealthConcernsAllergies() in nurse class
-	String currentAllergies;
-	String currentHealthConcerns;
-	
-
-	//Patient data from setExamResults in doctor class
-	String physcialExamResults;
+	double currentWeight, currentTemperature;
 	
 	//Array for storing messages in patient inbox 
-	//**TODO**
 	String[] messages = new String[10];
+	
 
-	private String currentImmunizations;
-	
-	
-	public Patient(String first, String last, String dateOfBirth, String phone, String id, int age) {
+	public Patient(String first, String last, String dateOfBirth, String phone, String id, int age, String pass) throws IOException {
 		
 		firstName = first;
 		lastName = last;
@@ -55,7 +28,8 @@ public class Patient {
 		phoneNumber = phone;
 		patientID = id;	
 		currentAge = age;
-		username = lastName.substring(0, 4) + firstName.substring(0, 4) + DOB.substring(0, 4);
+		password = pass;
+		
 	}
 	
 
@@ -68,36 +42,34 @@ public class Patient {
 	public void setCurrentHealthIssues(String healthIssues) {
 		currentHealthIssues = healthIssues;
 	}
-	public void setPassword(String pass) {
-		password = pass;
-	}
 	public void sendPatientMessage(String empID) {
 		//TODO
 		
 	}
 
 
+	
+	//Data Storage Format
 	public String[] generateStorageArray() {
 		String[] data = new String[arraySize];
 		data[0] = patientID;
-		data[1] = firstName;
-		data[2] = lastName;
-		data[3] = username;
-		data[4] = password;
-		data[5] = DOB;
-		data[6] = Integer.toString(currentAge);
-		data[7] = phoneNumber;
-		data[8] = insuranceInformation;
-		data[9] = pharmacyInformation;
-		data[10] = currentMedications;
-		data[11] = currentImmunizations;
-		data[12] = currentHealthIssues;
-		data[13] = Double.toString(currentWeight);
-		data[14] = Double.toString(currentTemperature);
-		data[15] = bloodPressure;
-		data[16] = currentAllergies;
-		data[17] = currentHealthConcerns;
-		data[18] = physcialExamResults;
+		data[1] = password;
+		data[2] = firstName;
+		data[3] = lastName;
+		data[4] = DOB;
+		data[5] = Integer.toString(currentAge);
+		data[6] = phoneNumber;
+		data[7] = insuranceInformation;
+		data[8] = pharmacyInformation;
+		data[9] = currentMedications;
+		data[10] = immunizations;
+		data[11] = currentHealthIssues;
+		data[12] = Double.toString(currentWeight);
+		data[13] = Double.toString(currentTemperature);
+		data[14] = bloodPressure;
+		data[15] = currentAllergies;
+		data[16] = currentHealthConcerns;
+		data[17] = physicalExamResults;
 		
 		return data;
 	}
